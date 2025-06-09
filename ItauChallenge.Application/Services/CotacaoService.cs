1acd1ebf1ea8b9b1c6129f8775312a41ba7a527b
Leandro Diomar Gomes da Silva <leandrodiomar123@gmail.com>
2025-06-09 07:55:57 -0300

    <feat> : Criação de services e interfaces na camada de Application

---
+++ ItauChallenge.Application/Services/CotacaoService.cs
@@ -0,0 +1,13 @@
+﻿using ItauChallenge.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItauChallenge.Application.Services
+{
+    public class CotacaoService : ICotacaoService
+    {
+    }
+}
